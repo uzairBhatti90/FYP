b150bc3d72e060572d587dc8f4503576e626b1f3
mujtabaRN12 <gmb17698@gmail.com>
2022-12-31 21:53:17 +0500

    update setting of linking components

--- AutoMobile_SOS/src/screens/app/home/home.js
+++ AutoMobile_SOS/src/screens/app/home/home.js
@@ -1,37 +1,42 @@
-import React, {Component, usestate} from 'react';
+import React, { Component, useState } from 'react';
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions";
-import { AppButton } from "../../components/gerenal/appButton";
-import { TxtInput } from "../../components/gerenal/txtinput";
-import { colors, fontFamily } from "../../globals/utilities";
+import { AppButton } from '../../../components/gerenal/appButton/index';
+import { TxtInput } from "../../../components/gerenal/txtinput/index";
+import { colors, fontFamily } from '../../../globals/utilities/index';
 import {
-View,
-Text,
-Button,
-TouchableOpacity,
-}from "react-native";
-
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 
 const Home = props => {
   const [Auto, setAuto] = useState("");
-    return (
-        <View>
-          <Text>
-            Rider
-          </Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text>
+          Rider
+        </Text>
+        <TxtInput
+          MyStyles={styles.inputStyleView}
+          itsStyle={styles.inputStyle}
+          placeholder="Auto-name"
+          onChangeText={text => setAuto(text)}
+        />
+       
 
-          <AppButton
-                    title={'Search nearest spot'}
-                    myStyles={styles.button}
-                    itsTextstyle={styles.buttonText}
-                    onPress={() => { props.navigation.navigate('Location')}}
-                />
-                 <TxtInput
-                    MyStyles={styles.inputStyleView}
-                    itsStyle={styles.inputStyle}
-                    placeholder="Auto-name"
-                    onChangeText={text => setAuto(text)}
-                />
-        </View>
+        <AppButton
+          title={'Search nearest spot'}
+          myStyles={styles.button}
+          itsTextstyle={styles.buttonText}
+          onPress={() => { props.navigation.navigate('LocationStackScreens') }}
+        />
+      
+      </View>
+    </View>
   )
 };
 
@@ -40,34 +45,39 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: "#fff",
+    flex: 1,
+    backgroundColor: "#fff",
   },
-  
+  innerContainer: {
+    marginTop: responsiveHeight(6),
+    width: responsiveWidth(90),
+    alignSelf: "center"
+  },
   inputStyleView: {
-      width: responsiveWidth(90),
-      alignSelf: "center",
-      backgroundColor: 'transparent',
-      borderBottomWidth: responsiveWidth(0.1)
+    width: responsiveWidth(90),
+    alignSelf: "center",
+    backgroundColor: 'transparent',
+    borderBottomWidth: responsiveWidth(0.1)
   },
   inputStyle: {
-      width: responsiveWidth(80),
-      color: 'black',
-      height: responsiveHeight(5.5)
+    width: responsiveWidth(80),
+    color: 'black',
+    height: responsiveHeight(5.5)
   },
 
   button: {
-      width: responsiveWidth(90),
-      alignItems: "center",
-      justifyContent: "center",
-      borderRadius: responsiveWidth(3),
-      height: responsiveHeight(7)
+    width: responsiveWidth(90),
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: responsiveWidth(3),
+    height: responsiveHeight(7)
   },
   buttonText: {
-      fontSize: responsiveFontSize(2),
-      fontFamily: fontFamily.appTextMedium,
-      color: colors.white
+    fontSize: responsiveFontSize(2),
+    fontFamily: fontFamily.appTextMedium,
+    color: colors.white
   },
-  
+ 
+
 });
 
--- AutoMobile_SOS/src/screens/app/location/location.js
+++ AutoMobile_SOS/src/screens/app/location/location.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { View,StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Platform } from "react-native";
 import { colors } from "../../../globals/utilities/colors";
+import { responsiveHeight, responsiveWidth, responsiveFontSize } from "react-native-responsive-dimensions";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
+
+
 
 const Location = props => {
-    return(
+    return (
         <View style={styles.container}>
-            <Text>Location</Text>
+            <View style={styles.mapContainer}>
+                <MapView
+                    provider={Platform.OS === 'android' ? PROVIDER_GOOGLE : null} // remove if not using Google Maps
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                </MapView>
+            </View>
         </View>
     )
 }
 export default Location
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-         color: colors.primary,
-         justifyContent:"center",
-         alignItems:"center"
+    container: {
+        flex: 1,
+        color: colors.primary,
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    mapContainer: {
+        // ...StyleSheet.absoluteFillObject,
+        height: responsiveHeight(100),
+        width: responsiveWidth(100),
+        justifyContent: 'flex-end',
+        alignItems: 'center',
     }
 })
